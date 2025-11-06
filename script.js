62f088a287b9b9c76816b540de8abf849af0cde1
zunildachipana <omichipana@gmail.com>
2025-11-06 01:51:16 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// Efecto suave de desplazamiento
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
